d0c10c243cb3ba56248323f1c50cccaa2495dfb6
Angelica <alstar555@gmail.com>
2021-10-21 20:15:30 -0400

    fixed to work on all screens

--- constants.py
+++ constants.py
@@ -2,3 +2,5 @@ GLOBAL_light_square = (170, 215, 81)
 GLOBAL_dark_square = (162, 209, 73)
 GLOBAL_border = (87, 138, 52)
 GLOBAL_apple = (207, 83, 47)
+GLOBAL_width = 16
+GLOBAL_height = 14
--- snake.py
+++ snake.py
@@ -2,106 +2,83 @@ from PIL import ImageGrab
 import pyautogui
 import webbrowser
 import time
-from constants import GLOBAL_border, GLOBAL_dark_square, GLOBAL_light_square, GLOBAL_apple
+from constants import GLOBAL_border, GLOBAL_dark_square, GLOBAL_light_square, GLOBAL_apple, GLOBAL_width
 
 
 def open_game():
     webbrowser.open("https://www.google.com/search?q=google+snake&rlz=1C1CHBF_enUS918US918&oq=google+snake&aqs=chrome.0.69i59j0i433i512j0i131i433i512l3j0i512j0i131i433i512j0i512l3.1487j0j7&sourceid=chrome&ie=UTF-8")
-    time.sleep(2)
+    time.sleep(3)
     pyautogui.click(x = 650, y = 931, clicks = 1, button = 'left')
     time.sleep(0.5)
     pyautogui.click(x = 906, y = 873, clicks = 1, button = 'left')
     time.sleep(0.5)
 
-def print_grid():
-    #square = 48x48 pxls
-    board_start = (606, 332)
-    coords = board_start
-    next_y = coords[1]
-    screenshot = pyautogui.screenshot()
-    screenshot = screenshot.resize((1920, 1080))
-    while coords[0] < 1430 and coords[1] < 1030:
-        #pyautogui.click(x = coords[0], y = coords[1], clicks = 0, button = 'left')
-        current = screenshot.getpixel(coords)
-            #print border/ keep in bounds
-        if coords[0] >= 1400:
-            print(".")
-            next_y = coords[1]+48
-            next_x = board_start[0]
-        else:
-            next_x = coords[0]+48
-            if current[2] >= 100: #print snake #blue(0, 0, 255)
-                print("~", end = "") 
-            elif current[1] >= 100: # green (0, 255, 0)
-                print("X", end="")
-            elif current[0] >= 100: #red (255, 0, 0)
-                print("@", end="") 
-        coords = (next_x, next_y)
 
-def grab_board(dims):
-    board = []
-    board_row = ""
-    #square = 48x48 pxls
-    board_start = (24, 24)
-    coords = board_start
-    next_y = coords[1]
-    screenshot = grab_and_crop(dims)
-    screenshot.show()
-    while coords[0] < 816 and coords[1] < 720:
-        pyautogui.click(x = coords[0], y = coords[1], clicks = 0, button = 'left')
-        current = screenshot.getpixel(coords)
-            #print border/ keep in bounds
-        if coords[0] >= 816:
-            board.append(board_row)
-            board_row = ""
-            next_y = coords[1]+48
-            next_x = board_start[0]
-        else:
-            next_x = coords[0]+48
-            if current[2] >= 100: #print snake #blue(0, 0, 255)
-                board_row += "1"
-            elif current[1] >= 100: # green (0, 255, 0)
-                board_row += "0"
-            elif current[0] >= 100: #red (255, 0, 0)
-                board_row += "2"
-        coords = (next_x, next_y)
-    return board
-
-# def grab_board():
-#     board = ""
+# def grab_board(dims):
+#     board = []
+#     board_row = ""
 #     #square = 48x48 pxls
-#     board_start = (558, 332)
+#     board_start = (24, 24)
 #     coords = board_start
 #     next_y = coords[1]
-#     screenshot = pyautogui.screenshot()
-#     screenshot = screenshot.resize((1920, 1080))
-#     width = 0
-#     height=0
-#     count =0
-#     while coords[0] < 1430 and coords[1] < 1030:
+#     screenshot = grab_and_crop(dims)
+#     while coords[1] < 720:
 #         #pyautogui.click(x = coords[0], y = coords[1], clicks = 0, button = 'left')
 #         current = screenshot.getpixel(coords)
 #             #print border/ keep in bounds
-#         if coords[0] >= 1400:
-#             width+=1
-#             height = count
+#         if coords[0] >= 792:
+#             board.append(board_row)
+#             board_row = ""
 #             next_y = coords[1]+48
 #             next_x = board_start[0]
 #         else:
 #             next_x = coords[0]+48
 #             if current[2] >= 100: #print snake #blue(0, 0, 255)
-#                 board += "1"
+#                 board_row += "1"
 #             elif current[1] >= 100: # green (0, 255, 0)
-#                 board += "0"
+#                 board_row += "0"
 #             elif current[0] >= 100: #red (255, 0, 0)
-#                 board += "2"
+#                 board_row += "2"
 #         coords = (next_x, next_y)
 #     return board
 
+def grab_board(dims):
+    board = ""
+    #square = 48x48 pxls
+    board_start = (24, 24)
+    coords = board_start
+    next_y = coords[1]
+    screenshot = grab_and_crop(dims)
+    while coords[1] < 720:
+        #pyautogui.click(x = coords[0], y = coords[1], clicks = 0, button = 'left')
+        current = screenshot.getpixel(coords)
+            #print border/ keep in bounds
+        if coords[0] >= 792:
+            next_y = coords[1]+48
+            next_x = board_start[0]
+        else:
+            next_x = coords[0]+48
+            if current[2] >= 100: #print snake #blue(0, 0, 255)
+                board += "1"
+            elif current[1] >= 100: # green (0, 255, 0)
+                board += "0"
+            elif current[0] >= 100: #red (255, 0, 0)
+                board += "2"
+        coords = (next_x, next_y)
+    return board
+
+# def print_board(board):
+#     for x in board:
+#         print(x)
+#     print()
 
 def print_board(board):
+    count=0
     for x in board:
-        print(x)
+        count+=1
+        print(x,end="")
+        if count%GLOBAL_width==0:
+            print()
     print()
 
 def move(dir):
@@ -114,32 +91,25 @@ def move(dir):
         pyautogui.press('left')
     elif dir=="d":
         pyautogui.press('down')
-    #time.sleep(.2)
 
-#coordinates of snake=1, apple=2
+#find coordinates of charecter in board
 # def find_in_board(charecter, board, snake_width):
-#     coord = -1
-#     coord_x = -1
 #     coord_y = -1
 #     for x in board:
 #         coord_y+=1
-#         coord_x = 0
-#         for y in x:
-#             coord_x+=1
-#             if y == charecter:
-#                 coord = (coord_x+snake_width, coord_y)
-#                 return coord
-#     return coord
-
+#         coord_x = x.find(charecter)
+#         if coord_x != -1:
+#             coord = (coord_x, coord_y)
+#             return coord
+#     return -1
 def find_in_board(charecter, board, snake_width):
-    coord_y = -1
-    for x in board:
-        coord_y+=1
-        coord_x = x.find(charecter)
-        if coord_x != -1:
-            coord = (coord_x, coord_y)
-            return coord
-    return -1
+    index = board.find(charecter)
+    if index != -1:
+        x = int(index%GLOBAL_width)
+        y = index//GLOBAL_width
+        coords = (x, y)
+        return coords
+    return index
 
 #direction snake should move towards apple
 def dir_to_apple(current_dir, apple, snake):
@@ -164,7 +134,7 @@ def move_to_apple(current_dir, board, snake_width, dims):
     apple_pos = find_in_board("2", board, snake_width) #apple = 2
     gameover = False;
     while(apple_pos != -1 and not gameover):
-        gameover =  check_gameover()
+        gameover =  check_gameover(dims)
         direction_to_apple = dir_to_apple(current_dir, apple_pos, snake_pos)
         current_dir = direction_to_apple
         move(direction_to_apple) # r, u, l, d
@@ -173,11 +143,10 @@ def move_to_apple(current_dir, board, snake_width, dims):
         apple_pos = find_in_board("2", board, snake_width) #apple = 1
     return gameover
     
-def check_gameover():
+def check_gameover(dims):
     gameover = False
-    screenshot = pyautogui.screenshot()
-    screenshot = screenshot.resize((1920, 1080))
-    value = screenshot.getpixel((1045, 415))
+    screenshot = grab_and_crop(dims)
+    value = screenshot.getpixel((505, 105))
     if value[0] >= 200 and value[1]>= 190:
         gameover = True
     elif value == (12, 12, 12):
@@ -185,10 +154,8 @@ def check_gameover():
     return gameover
     
 
-def grab_and_crop(dim):
-    # screenshot = pyautogui.screenshot()
-    # screenshot = screenshot.resize((816,720))
-    return ImageGrab.grab().crop(dim).resize((816,720))
+def grab_and_crop(dims):
+    return ImageGrab.grab().crop(dims).resize((816,720))
 
 def find_edge(screen, pos, pval, direction):
     """
@@ -247,14 +214,13 @@ if __name__=="__main__":
     move(current_dir)
     time.sleep(.5)
     board = grab_board(dims)
-    print_board(board)
 
     #logic
     gameover = False
     while(not gameover):
         gameover = move_to_apple(current_dir, board, snake_width, dims)
         board = grab_board(dims)
-        #print_board(board)
+        # print_board(board)
         snake_width+=1
        
     
